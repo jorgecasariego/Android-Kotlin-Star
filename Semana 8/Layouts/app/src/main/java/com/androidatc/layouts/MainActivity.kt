5ef8bdec2ff76478abeea54150e74f6c5eaf6585
Jorge Casariego <jorgekasa@gmail.com>
2018-07-17 12:12:29 -0400

    Semana 8

---
+++ Semana 8/Layouts/app/src/main/java/com/androidatc/layouts/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.androidatc.layouts
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ejercicio_relative_layout)
+    }
+}
